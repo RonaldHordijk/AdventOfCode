912408b6e578d8e793c6ac85611bc4c2706749e0
ronald.hordijk@tno.nl <ronald.hordijk@tno.nl>
2022-12-04 08:38:20 +0100

    Add day 3 and 4

---
+++ AdventOfCode2022/Day3/Program.cs
@@ -0,0 +1,40 @@
+﻿var lines = File.ReadAllLines("data.txt");
+
+var sum = 0;
+
+foreach (var line in lines)
+{
+    var dub = GetDouble(line.Substring(0, line.Length / 2), line.Substring(line.Length / 2));
+    sum += GetValue(dub);
+}
+Console.WriteLine($"Total Score {sum}");
+
+sum = 0;
+for (int i = 0; i < lines.Length; i += 3)
+{
+    var dub = GetCommon(lines[i], lines[i + 1], lines[i + 2]);
+    sum += GetValue(dub);
+}
+Console.WriteLine($"Total Score badges {sum}");
+
+int GetValue(char dub)
+{
+    if (Char.IsUpper(dub))
+    {
+        return dub - 'A' + 27;
+    }
+    else
+    {
+        return dub - 'a' + 1;
+    }
+}
+
+char GetDouble(string v1, string v2)
+{
+    return v1.ToCharArray().First(c => v2.Contains(c));
+}
+
+char GetCommon(string v1, string v2, string v3)
+{
+    return v1.ToCharArray().First(c => v2.Contains(c) && v3.Contains(c));
+}
---
+++ AdventOfCode2022/Day4/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = File.ReadAllLines("data.txt");
+
+var sum = 0;
+var sum2 = 0;
+
+foreach (var line in lines)
+{
+    var elfs = line.Split(",");
+    var elf1 = elfs[0].Split("-");
+    var elf2 = elfs[1].Split("-");
+
+    int elf1Start = int.Parse(elf1[0]);
+    int elf1End = int.Parse(elf1[1]);
+    int elf2Start = int.Parse(elf2[0]);
+    int elf2End = int.Parse(elf2[1]);
+
+    if ((elf1Start <= elf2Start && elf1End >= elf2End)
+        || (elf2Start <= elf1Start && elf2End >= elf1End))
+        sum++;
+
+    if (!((elf1Start > elf2End) || (elf1End < elf2Start)))
+        sum2++;
+}
+
+Console.WriteLine($"Total overlapping {sum}");
+Console.WriteLine($"Total Partial overlapping {sum2}");
