ecc00a6655982ba749e88e10624e23d58281de33
ronald.hordijk@tno.nl <ronald.hordijk@tno.nl>
2023-11-12 09:14:41 +0100

    Add more days

---
+++ AdventOfCode2022/Day13/Program.cs
@@ -0,0 +1,161 @@
+﻿using Day13;
+
+var lines = File.ReadAllLines("data.txt");
+
+int index = 1;
+int sum = 0;
+
+for (int i = 0; i < lines.Length; i += 3)
+{
+    var l1 = Parse(lines[i]);
+    var l2 = Parse(lines[i + 1]);
+
+    if (Compare(l1, l2) < 0)
+    {
+        Console.WriteLine("Wrong order");
+    }
+    else
+    {
+        Console.WriteLine("Right order");
+        sum += index;
+    }
+
+    index++;
+}
+
+Console.WriteLine($"sum is {sum}");
+
+List<ListElement> elements = new();
+for (int i = 0; i < lines.Length; i += 3)
+{
+    elements.Add(Parse(lines[i]));
+    elements.Add(Parse(lines[i + 1]));
+}
+
+var e2 = Parse("[[2]]");
+var e6 = Parse("[[6]]");
+elements.Add(e2);
+elements.Add(e6);
+
+elements.Sort(Compare);
+elements.Reverse();
+
+int i2 = elements.IndexOf(e2);
+int i6 = elements.IndexOf(e6);
+
+Console.WriteLine($"loc e2 {i2} e6 {i6} = tot {(i2 + 1) * (i6 + 1)}");
+
+
+int Compare(ListElement l1, ListElement l2)
+{
+    if (!l1.IsList && !l2.IsList)
+        return Math.Sign(l2.Value - l1.Value);
+
+    if (l1.IsList && !l2.IsList)
+    {
+        return Compare(l1, new ListElement { IsList = true, List = { l2 } });
+    }
+
+    if (!l1.IsList && l2.IsList)
+    {
+        return Compare(new ListElement { IsList = true, List = { l1 } }, l2);
+    }
+
+    if (l1.IsList && l2.IsList)
+    {
+        int nrItem = Math.Min(l1.List.Count, l2.List.Count);
+
+        for (int i = 0; i < nrItem; i++)
+        {
+            int res = Compare(l1.List[i], l2.List[i]);
+            if (res != 0)
+                return res;
+        }
+
+        if (l2.List.Count == l1.List.Count)
+            return 0;
+
+        return Math.Sign(l2.List.Count - l1.List.Count);
+    }
+
+    return 0;
+}
+
+ListElement Parse(string v)
+{
+
+    if (IsList(v))
+    {
+        // list
+        string listtext = v = v[1..^1];
+        ListElement result = new ListElement { IsList = true };
+        while (!string.IsNullOrEmpty(listtext))
+        {
+            (string element, string rest) = GetElement(listtext);
+
+            result.List.Add(Parse(element));
+
+            listtext = rest;
+        }
+
+        return result;
+    }
+
+    return new ListElement
+    {
+        Value = int.Parse(v),
+    };
+}
+
+bool IsList(string v)
+{
+    return v.StartsWith("[") && v.EndsWith("]");
+}
+
+(string element, string rest) GetElement(string v)
+{
+    if (v.StartsWith("["))
+    {
+        var indexEnd = GetClosingIndex(v) + 1;
+        if (indexEnd == v.Length)
+        {
+            return (v, string.Empty);
+        }
+
+        return (v[..indexEnd], v[(indexEnd + 1)..]);
+    }
+
+    // text
+    int i = v.IndexOf(",");
+    if (i < 0)
+    {
+        return (v, string.Empty);
+    }
+    else
+    {
+        return (v[..i], v[(i + 1)..]);
+    }
+
+}
+
+int GetClosingIndex(string v)
+{
+    int indent = 0;
+
+    for (int i = 0; i < v.Length; i++)
+    {
+        if (v[i] == '[')
+        {
+            indent++;
+        }
+
+        if (v[i] == ']')
+        {
+            indent--;
+            if (indent == 0)
+                return i;
+        }
+    }
+
+    return 0;
+}
---
+++ AdventOfCode2022/Day15/Program.cs
@@ -0,0 +1,85 @@
+﻿using Day15;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("data.txt");
+
+List<Sensor> sensors = new();
+
+//Sensor at x=20, y=1: closest beacon is at x=15, y=3
+Regex regex = new Regex(@".*x=([+-]?\d+).*y=([+-]?\d+).*x=([+-]?\d+).*y=([+-]?\d+)");
+
+foreach (var line in lines)
+{
+    Match match = regex.Match(line);
+
+    // Step 3: test for Success.
+    if (match.Success)
+    {
+        sensors.Add(new Sensor
+        {
+            SX = int.Parse(match.Groups[1].Value),
+            SY = int.Parse(match.Groups[2].Value),
+            BX = int.Parse(match.Groups[3].Value),
+            BY = int.Parse(match.Groups[4].Value),
+        });
+    }
+}
+
+var regions = GetRegions(10);
+int sum = 0;
+
+foreach (var r in regions)
+    sum += (r.e - r.s);
+
+Console.WriteLine($"sum in {sum}");
+
+for (int r = 0; r < 4000000; r++)
+{
+    var rr = GetRegions(r);
+    if (rr.Count > 1)
+    {
+        Console.WriteLine(r);
+        foreach (var region in rr)
+            Console.WriteLine($"s = {region.s}, e ={region.e}");
+        Console.WriteLine($"v = {(long)4000000 * ((long)rr[0].e + 1) + (long)r}");
+        Console.WriteLine($"v = {4000000 * r + (rr[0].e + 1)}");
+    }
+}
+
+List<(int s, int e)> GetRegions(int row)
+{
+
+    List<(int s, int e)> regions = new();
+
+    foreach (var sensor in sensors)
+    {
+        (int s, int e) = sensor.GetRow(row);
+
+        if (e >= s)
+            regions.Add((s, e));
+        //        Console.WriteLine($"s = {s}, e ={e}");
+    }
+
+    for (int i = 0; i < regions.Count; i++)
+    {
+        //Console.WriteLine("i");
+        //foreach (var r in regions)
+        //    Console.WriteLine($"s = {r.s}, e ={r.e}");
+
+        for (int j = i + 1; j < regions.Count; j++)
+        {
+            if (regions[i].e < regions[j].s || regions[j].e < regions[i].s)
+                continue;
+
+            // join
+            regions[i] = (Math.Min(regions[i].s, regions[j].s), Math.Max(regions[i].e, regions[j].e));
+            regions.RemoveAt(j);
+            j = i;
+
+            //Console.WriteLine();
+            //foreach (var r in regions)
+            //    Console.WriteLine($"s = {r.s}, e ={r.e}");
+        }
+    }
+    return regions;
+}
