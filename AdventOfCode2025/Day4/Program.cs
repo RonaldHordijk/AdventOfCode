4349c8889a4945b01f111536e4f6724847ac56ac
Ronald Hordijk <ronald.hordijk@tno.nl>
2025-12-05 08:20:12 +0100

    Day 4 and 5

---
+++ AdventOfCode2025/Day4/Program.cs
@@ -0,0 +1,48 @@
+﻿var lines = File.ReadAllLines("data.txt");
+
+int xcount = lines[0].Length;
+int ycount = lines.Length;
+
+char[][] chars = lines.Select(s => s.ToCharArray()).ToArray();
+
+
+
+int count = 0;
+
+bool found = true;
+while (found)
+{
+    found = false;
+
+
+    for (int x = 0; x < xcount; x++)
+    {
+        for (int y = 0; y < ycount; y++)
+        {
+            int nb = 0;
+
+            if (chars[x][y] != '@')
+                continue;
+
+            for (int i = Math.Max(x - 1, 0); i <= Math.Min(x + 1, xcount - 1); i++)
+            {
+                for (int j = Math.Max(y - 1, 0); j <= Math.Min(y + 1, ycount - 1); j++)
+                {
+                    if (chars[i][j] == '@')
+                    {
+                        nb++;
+                    }
+                }
+            }
+
+            if (nb < 5)
+            {
+                count++;
+                chars[x][y] = '.';
+                found = true;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"count {count}");
---
+++ AdventOfCode2025/Day5/Program.cs
@@ -0,0 +1,94 @@
+﻿var lines = File.ReadAllLines("data.txt");
+
+
+List<(long, long)> rules = [];
+
+foreach (var line in lines)
+{
+    if (line.Contains('-'))
+    {
+        var words = line.Split('-');
+        rules.Add((Convert.ToInt64(words[0]), Convert.ToInt64(words[1])));
+    }
+}
+
+int cnt = 0;
+
+foreach (var line in lines)
+{
+    if (!line.Contains('-') && line.Length > 0)
+    {
+        long val = Convert.ToInt64(line);
+        bool spoiled = true;
+        foreach (var rule in rules)
+        {
+            if (val >= rule.Item1 && val <= rule.Item2)
+            {
+                spoiled = false;
+                break;
+            }
+        }
+
+        if (!spoiled)
+        {
+            cnt++;
+        }
+    }
+}
+
+
+Console.WriteLine($"sumorder {cnt}");
+
+bool found = true;
+while (found)
+{
+    found = false;
+    for (int i = 0; i < rules.Count; i++)
+    {
+        for (int j = 0; j < rules.Count; j++)
+        {
+            if (i == j)
+                continue;
+
+            if ((rules[j].Item1 >= rules[i].Item1 && rules[j].Item1 <= rules[i].Item2)
+                || (rules[j].Item2 >= rules[i].Item1 && rules[j].Item2 <= rules[i].Item2))
+
+            {
+                rules[i] = (Math.Min(rules[i].Item1, rules[j].Item1),
+                            Math.Max(rules[i].Item2, rules[j].Item2));
+                rules.RemoveAt(j);
+                found = true;
+                break;
+            }
+        }
+    }
+}
+
+cnt = 0;
+foreach (var line in lines)
+{
+    if (!line.Contains('-') && line.Length > 0)
+    {
+        long val = Convert.ToInt64(line);
+        bool spoiled = true;
+        foreach (var rule in rules)
+        {
+            if (val >= rule.Item1 && val <= rule.Item2)
+            {
+                spoiled = false;
+                break;
+            }
+        }
+
+        if (!spoiled)
+        {
+            cnt++;
+        }
+    }
+}
+
+
+Console.WriteLine($"sumorder {cnt}");
+
+
+Console.WriteLine($"rule {rules.Sum(r => r.Item2 - r.Item1 + 1)}");
